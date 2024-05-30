30f2a3c5f50408f0ddbde84e4c9b618c19c54d55
YamanaShop <159576695+YamanaShop@users.noreply.github.com>
2024-05-30 21:25:31 +0200

    third commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/YamanaShop/hunde
+
+go 1.22.2
---
+++ hunde.go
@@ -0,0 +1,9 @@
+package hunde
+
+import (
+	"strings"
+)
+
+func WhenGrownUp(st string) string {
+	return "When the puppy grows up it says: " + strings.ToUpper(st)
+}
